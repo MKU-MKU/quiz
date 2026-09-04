10fd49b884444f11ae127c9d75796373120f3dc5
aaamit461-pixel <aaamit461@gmail.com>
2026-09-04 20:40:24 +0545

    Update chapters-data.js

--- chapters-data.js
+++ chapters-data.js
@@ -179,7 +179,8 @@ const DRIVE = {
     },
     "6": {
       "New DPARSAD": { "1-75": "1cQGpQHGzekcDnE2duOkYzx3NAoeuPv5h", "76-150": "1T2tghXwm_6Dqy5FkQgs0wiRrhl0NQuGM", "151-225": "1tro7AirSkoOm9zYvJyQo5W_hlUvoqvlg", "225-310": "12jhiq9Jbp3EwvqJgW_bxyPq77IYfUEAa" },
-       "Old DPRASAD ":{ "1-50": "1IYG4gFrvXBJ8n2kJRFL1UCRZdUOZsZCv", "50-100": "1981sMj5WNTZeKsghbz4N_eEqqxm1XbT5", "100-150": "1KY9GTqB8sTJpGha_JnYYE9Pvnnzotf9h", "150-200": "18HnjG1-leT6OXk5mYPGTJluMw2yiFUEy","200-300": "1KA-3P2cmmQKQRmr_-UU74ik_MmNsJ8F2" }
+       "Old DPRASAD ":{ "1-50": "1IYG4gFrvXBJ8n2kJRFL1UCRZdUOZsZCv", "50-100": "1981sMj5WNTZeKsghbz4N_eEqqxm1XbT5", "100-150": "1KY9GTqB8sTJpGha_JnYYE9Pvnnzotf9h", "150-200": "18HnjG1-leT6OXk5mYPGTJluMw2yiFUEy","200-300": "1KA-3P2cmmQKQRmr_-UU74ik_MmNsJ8F2" },
+        "RK SHRESTHA ":{ "1-67": "1_KH4l09hI0GeH6J5tGd6vGCQZzNjj7_J", "67-134": "1V9EtSiGB2_jaOpTdna_0VGt6iSVh6WmG", "134-201": "1vdN4MvatiJJ-7Qjz4epjV6FluEVf5a-m", "201-268": "1nz3Tvjl6THGuXYKTnXLH8ZbYKxyfRy-w","268-335": "1CJfsghP6UOy5qv29ktUrhhNBhqnmqRj7" }
     },
     "7": {
       "DPARSAD": { "1-50": "1WR0c-cQrD6ZNrpW31pgFTrhekyT_0n4K", "51-100": "1RjkK83GYpLncIqJJ2FGYkHh0FGGksJ_J", "101-150": "1O3bhzDvGZfUTy1T9guFuq_aAn3PAq7Xi", "151-200": "1E22sJNC6miJwVNDD5cz8EW3OXTWKc4XT", "201-250": "1-VeNFb81ynETihERKfWNUOZSqrYopyOH" },
